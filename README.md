6678ebc0ac832370ffb1167709847ea419a33f44
Alexander Weigl <spamling@web.de>
2015-01-03 19:52:57 +0100

    update readme, entry into cheeseshop

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 murmgr
 ======
 
+[![Downloads](https://pypip.in/download/murmgr/badge.svg)](https://pypi.python.org/pypi/murmgr/)
+[![Latest Version](https://pypip.in/version/murmgr/badge.svg)](https://pypi.python.org/pypi/murmgr/)
+[![Supported Python versions](https://pypip.in/py_versions/murmgr/badge.svg)](https://pypi.python.org/pypi/murmgr/)
+[![Development Status](https://pypip.in/status/murmgr/badge.svg)](https://pypi.python.org/pypi/murmgr/)
+[![License](https://pypip.in/license/murmgr/badge.svg)](https://pypi.python.org/pypi/murmgr/)
+
 Management of MSML User Packages and Repositories
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='murmgr',
-    version='0.1',
-    packages=[''],
+    version='1.0.0-beta',
     url='https://github.com/CognitionGuidedSurgery/murmgr',
     license='GPL v3',
+    py_modules=['murmgr'],
     author='Alexander Weigl',
     author_email='uiduw@student.kit.edu',
     description='Management of MSML User Packages and Repositories',
@@ -14,4 +14,8 @@ setup(
         [console_scripts]
         murmgr=murmgr:cli
     ''',
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    )
 )
